980cbbdbd9f6c04eb5b629860588eb73459fc4b9
Manuel Guzman <m_guzman74@yahoo.com>
2021-06-16 00:55:58 -0500

    removed component from reciepreview that caused to many api calls

--- client/src/Components/RecipePreview/RecipePreview.js
+++ client/src/Components/RecipePreview/RecipePreview.js
@@ -1,16 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from './RecipePreview.module.css';
 import PropTypes from 'prop-types';
-import { getRecipeById } from '../../Utils/apiCalls';
-import RecipeMisc from './RecipeMisc';
 
 const RecipePreview = props => {
-  const [previewInfo, setPreviewInfo ] = useState({})
-  useEffect(async () => {
-    const data = await getRecipeById(props.itemId);
-    setPreviewInfo(data)
-  },[] )
-  console.log(previewInfo)
   return(
     <div className={styles.mainWrapper} >
       <div className={styles.imageWrapper}>
@@ -18,7 +10,6 @@ const RecipePreview = props => {
       </div>
       <div className={styles.infoWrapper}>
         <div className={styles.title}>{props.title}</div>
-        <RecipeMisc miscInfo={previewInfo} />
       </div>
     </div>
   )
@@ -28,7 +19,6 @@ const RecipePreview = props => {
 RecipePreview.propTypes = {
   imageSrc: PropTypes.string,
   title: PropTypes.string,
-  itemId: PropTypes.number
 }
 
 
